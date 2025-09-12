b5bfafe55bd5b437dd8390300fd4851175a6eb19
Fatima Oughella <fatima-zahra.oughella@grenoble-inp.org>
2025-09-12 13:04:25 +0200

    header and footer in the layout

--- app/newsletter/page.tsx
+++ app/newsletter/page.tsx
@@ -1,13 +1,9 @@
 // app/newsletter/page.tsx
 import { getPage } from "@/lib/page";
 import HomeBlocks from "@/components/blocks/home-blocks";
-import SiteHeader from "@/components/layout/site-header";
-import SiteFooter from "@/components/layout/site-footer";
-import { getLayout } from "@/lib/layout";
 
 
 export default async function NewsletterPage() {
-     const { site, footer } = await getLayout();
   
 const page = await getPage("newsletter");
 
